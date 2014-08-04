98447ba12f7d43feda38b6f24d1af9140c421a7b
Derek Greentree <derek@github.com>
2014-08-04 17:51:03 -0600

    seed randomness on each run

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"math/rand"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	experiment := NewExperiment("console")
-	experiment.AddBandit(NewBandit("control-pre-avatar", 15076, 481687))
-	experiment.AddBandit(NewBandit("test-pre-avatar", 5011, 161368))
+	experiment.AddBandit(NewBandit("control-avatar", 42060, 496653))
+	experiment.AddBandit(NewBandit("test-avatar", 15280, 166337))
 
 	winner := pickOptimalVariant(experiment, 1000)
 	fmt.Printf("%v\n", winner)
